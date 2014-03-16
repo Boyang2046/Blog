1f414ea4d302a0c2014669bd82cabebbb7b8598f
Boyang Li <bryanli2009@live.ca>
2014-03-16 19:02:03 -0400

    Add devise model 'User'.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140315231834) do
+ActiveRecord::Schema.define(version: 20140316224504) do
 
   create_table "comments", force: true do |t|
     t.string   "commenter"
@@ -28,19 +28,27 @@ ActiveRecord::Schema.define(version: 20140315231834) do
     t.text     "text"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "user_id"
+    t.integer  "users_id"
   end
 
-  add_index "posts", ["user_id"], name: "index_posts_on_user_id", using: :btree
+  add_index "posts", ["users_id"], name: "index_posts_on_users_id", using: :btree
 
   create_table "users", force: true do |t|
-    t.string   "email"
-    t.string   "password"
-    t.string   "username"
-    t.string   "user_type"
-    t.boolean  "locked"
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
